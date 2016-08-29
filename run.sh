ec515f441128da5df7b8929ec1dff207ab65e1ff
Scott Ouellette <scottx611x@gmail.com>
2016-08-29 21:59:32 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,16 +1,22 @@
+#!usr/bin/bash
+
+# sudo dpkg-reconfigure dash -> select NO!
+
 sudo killall python
 sudo rm -rf nohup.out
 mkdir -p vids
 mkdir -p pics
-pip install virtualenvwrapper==4.7.0
-source "/usr/bin/virtualenvwrapper.sh"
-export WORKON_HOME="/opt/virtual_env/"
-mkvirtualenv py_sPi
-workon py_sPi
+
+#pip install virtualenvwrapper==4.7.0
+
+#export PATH=/usr/local/bin:$PATH
+#source /usr/local/bin/virtualenvwrapper.sh
+
+#mkvirtualenv py_sPi
+#workon py_sPi
 pip install -r requirements.txt
-#sudo nohup python -m SimpleHTTPServer 7777 &
-sudo nohup python flask_server.py
+sudo nohup python flask_server.py &
 sleep 5
 nohup python py_sPi.py &
-sudo tail -f nohup
+sudo tail -f nohup.out
 
