f63dee8c81844a1a80967be8ceff24a60e28e870
Scott Ouellette <scottx611x@gmail.com>
2016-08-29 15:20:31 +0000

    update run script

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ sudo rm -rf nohup.out
 mkdir -p vids
 mkdir -p pics
 pip install virtualenvwrapper==4.7.0
+source "/usr/bin/virtualenvwrapper.sh"
+export WORKON_HOME="/opt/virtual_env/"
 mkvirtualenv py_sPi
 workon py_sPi
 pip install -r requirements.txt
@@ -10,5 +12,5 @@ pip install -r requirements.txt
 sudo nohup python flask_server.py
 sleep 5
 nohup python py_sPi.py &
-sudo tail -f nohup.out
+sudo tail -f nohup
 
