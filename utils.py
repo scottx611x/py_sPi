8ea56c5c77c198ccd0732e1bee37f1efa9470853
Scott Ouellette <scottx611x@gmail.com>
2016-12-27 19:46:03 -0500

    Refactor utils

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Scott Ouellette | scottx611x@gmail.com
-
 # --------------------------------------
 import ephem
+import threading
 
 
 def day_or_night_pi():
@@ -14,3 +14,15 @@ def day_or_night_pi():
         return "DAY_PI"
     else:
         return "NIGHT_PI"
+
+
+def run_in_thread(fn):
+    """
+    :param fn: function to be run in its own thread
+    :return: thread object
+    """
+    def run(*k, **kw):
+        t = threading.Thread(target=fn, args=k, kwargs=kw)
+        t.start()
+        return t
+    return run
